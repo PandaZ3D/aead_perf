5b34ba45fc6f3fb8fe4820c41c4465d25f8e4bd8
Allen Aboytes <aaboytes@ucsc.edu>
2021-03-08 08:37:16 +0000

    file generation script

--- bench/bench_setup.py
+++ bench/bench_setup.py
@@ -3,11 +3,32 @@
 
   Setup date to be encrypted during benchmarking tests
 '''
+import os
 
 def generate_random_block(block_size):
     # do stuff to make random block of data
-    random_block = '' # /dev/random
+    random_block = b'hi' # /dev/random
     return random_block
 
-block_sizes = [0, 1]
+def main():
+  # data sizes to generate
+  block_sizes = [0, 1]
 
+  # create directory where test data goes
+  data_dir = 'inputs'
+  if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+
+  # generate random amount of data
+  for b in block_sizes:
+    # open file handle where data will go
+    test_data_file = open(f"{data_dir}/input_{b}.dat", 'wb')
+    # generate random block of data
+    random_data = generate_random_block(b)
+    # write output data to file
+    test_data_file.write(random_data)
+    # close file reference (save)
+    test_data_file.close()
+
+if __name__ == '__main__':
+  main()
--- bench/measure_ciphers.sh
+++ bench/measure_ciphers.sh
@@ -3,7 +3,18 @@
 # info: measure both AES and ChaCha20 ciphers
 # run from within bench directory
 
+# setup python virtual environment
+# mkvirtualenv --python=$(which python3) pybench && \
+    # python -m pip install -r requirements.txt
+
+# use python environment
+# workon pybench
+source pybench/bin/activate
+
 # one time set-up for generating data
+python bench_setup.py
+
+deactivate
 
 # run bench for AES
 
